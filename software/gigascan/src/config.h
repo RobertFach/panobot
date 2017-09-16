fc234315d0ae6c7d8ceec9ba48a6b6b7c92c1144
Robert Fach <robert.fach@gmx.net>
2017-09-16 12:53:40 +0200

    updated configuration section, fixed issue with displaying status message, reorganized/commented code

--- software/gigascan/src/config.h
+++ software/gigascan/src/config.h
@@ -1,17 +1,21 @@
-const int PAN_STEP_PIN  = 24;
-const int PAN_DIR_PIN   = 25;
-const int TILT_STEP_PIN = 26;
-const int TILT_DIR_PIN = 27;
-const int FOCUS_PIN    = 22;
-const int SHUTTER_PIN  = 23;
+//Stepper Pins for Pan Stepper
+#define PAN_STEP_PIN  24
+#define PAN_DIR_PIN 25
+//Stepper Pins for Tilt Stepper
+#define TILT_STEP_PIN 26
+#define TILT_DIR_PIN 27
+//Pins to the Optocopplers for focus & shutter
+#define FOCUS_PIN 22
+#define SHUTTER_PIN 23
 
-#define LEDPIN LED_BUILTIN
+//SmartLCD display pins, for hardware SPI use these, CS can be rerouted
 #define DISPLAY_CLOCK_PIN 52 //CLK
 #define DISPLAY_DATA_PIN 51 //MOSI
 #define DISPLAY_CS_PIN 34
 
+//SmartLCD clickencoder pins, ecoder a/b uses PCINT, so make sure that its supported
 #define ENCODER_A_PIN A8
 #define ENCODER_B_PIN A9
-#define ENCODER_BTN_PIN -44 //negative means active internal pull-up
-
+//negative means active internal pull-up
+#define ENCODER_BTN_PIN -44
 #define DISPLAY_RESET_BTN_PIN -45 //negative means active internal pull-up
